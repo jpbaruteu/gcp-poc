21f037b2fa363ba9ab622418972cb42ae5bdc884
JP Baruteu <jbaruteu@gmail.com>
2020-10-28 14:47:10 +0000

    remove TF config

--- tf/project.tf
+++
@@ -1,9 +0,0 @@
-resource "google_project_service" "cloudbuild" {
-  service = "cloudbuild.googleapis.com"
-}
-resource "google_project_service" "run" {
-  service = "run.googleapis.com"
-}
-resource "google_project_service" "sourcerepo" {
-  service = "sourcerepo.googleapis.com"
-}
--- tf/repository.tf
+++
@@ -1,7 +0,0 @@
-resource "google_sourcerepo_repository" "default" {
-  name = "${var.project_id}-repository"
-
-  depends_on = [
-    google_project_service.sourcerepo,
-  ]
-}
